b74eb9675f534c1914cecd7ddba29e070c023065
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-05-05 14:56:37 -0500

    Update content paths in Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./public/index.html', './public/src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
