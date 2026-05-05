d2afed1df56d85bb06b9bf7a0c8b6ba47e81410d
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-05-05 13:29:22 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./public/index.html', './public/src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
