11bfd236bbbeec79997b1a649a4352ce80a1d623
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-05-10 00:07:50 -0500

    Add files via upload

---
+++ src/constants/src_constants_avatars.ts
@@ -0,0 +1,22 @@
+export interface PresetAvatar {
+  id: string;
+  url: string;
+}
+
+export const PRESET_AVATARS: PresetAvatar[] = [
+  { id: 'pollo-oficial-nuevo', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEh7V7gTJybwCUH08AWqQBFV5CpeTfI5S2wPHtHY7Rh3vTLsglqmckHzFCDXqDOvUvVlGgQY-2lduDqTmaZ4fpj0MaJjfCbPWqIqGQK119209LDzrpkIZqeL0VWDg1ibQBfQj65pM3iOK4JdBB0w-eX8LlyrIeigGkv0zPPrhudOEKvZahRPhcf7TVpEuc' },
+  { id: 'chef', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEh7EsqKk5qbx_tIh8kjD-rL22uRJnIGMgXCEDmmopSTWLOe_NHRvtk6no_8n5ZzDFHO5Khr-P1yYFoRBkCacNui9m1QiG4_utanMsqhC-0q5_ihkuTq_skSj0I2shSZNAFTcV4BZafwtkJoGvdD-NQ1dEAuUXnsuY7xrTw2jihj2FhDUeCV-BKsihuAmJQ' },
+  { id: 'ejecutor', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEh9d3CwJrF2deIWpYHloOmEVO5FAjGajvEcHbLKIjGdHOH8PFektvuXOFeKsqjK_JzQa5mhVP5vVQy5IapBEsC0fVkBrRl2MJorA9E0pDWBnPZLMheJk70DXhM5snaSJez0YNp0rg1nOpdUdhgBHjrc4ahnXhbhtaBb9kZDEL88a8m2gNr_NkZIjSunDqk' },
+  { id: 'tortuga-gafas', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEjJCqQcQCoY34MPamz7zMXfxpCMDcIZP128ZPdVrJFd41Hgi_bdujVFEdCVLrAYGRcUXfoApXmVqr182LQB5muE8Q0k9hy7P-Z16hXaQcDNGT4iBar-9l2pSYUiarLAcWV9quZR02DKBIPQtj45782BwSsiG7r3TfeyfrNwA2YeOycY5drvmSxGlvjJjMQ' },
+  { id: 'lobo-surf', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEjwpwMK9rRlrWzsajco076LLSQ8tH01fsiusGuWUoLjQj0haExFACwKVMIaXirUFKvtRq3U12QQw-Kodfs7_NkYRg4WCz63blYaHdMBVjw3n0iNMiSoHb2dGNEdIM5kroYIFEdOWc-bci4UXxMFm8K70o1l63hYBSPmpqFFfUf1NkYBpwgDxhnyzS-yf8I' },
+  { id: 'tropical-coco', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEhmYQRTqca3s6ZqD2IBz0K8mFfB1vFn2tqfK2zejCg4EwRDWeCQKSt4sWKe403Pz9E-fac78x-wRTVtLrHhO6PDSjML18swMYB2EmBizo-tCuU9IGhMcLM0k2JSSwotywgkfVDqqSiWj2efsawRlHWsWoa4EjciNo9sxFAlD2Pwk_pKgeN03UqXZmnZUM' },
+  { id: 'cabeza-pollito', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEhED6ZY2E2m2Yt-vDCtZq7__LwQ2Zt3W9L-OnI3NekEdfOUI722p5LDtMQQ9xvjOmx50aZWKoG9b3RkMWk7Lgc8pJy2o8XiujA1ra_lVgymiGBn0LNs9paDOj0NR12cqR_fTxwl-iOtTnEtBmc4-jNeVjMQ56wFlg-IxTSRwi8mAKTX43d8ApCGNYP_lm8' },
+  { id: 'pinguina-nueva', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEisBpMblsfUyn1M-xPeQyYvur0HdBuj3O2TtFwO2bb9Y1UzMn5EAP4B8OPsUluiF5oGeP8HrH4lzg29K30Za-91ouL8n_ntvu25bP-uy8ofV08WBiXm6gwdwCUWTvMPaR-tHDxhnZMOUGfTLh2tkxZXEVzp6BkyhzQ4ou7UuIz2FmR9KkuH4h1MMXEqz6c' },
+  { id: 'pollita-rosa', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEh3uIpzix7-7JRyqM6l5CQ9mPUwrIt8iHQC_Wm3LNjGBd43CheMXFcfiq7jvpDUzsh925MAHWnDeaCEJxb7r2xK0PnzzRu3QFScqqrrBz78lopFkThdt1m3nu1VuvY0Xd_hGzN8a8bZaoj-g4VokWEgdpf7vlBl5nkVGdhbuL3855-egEN7VjLvX4Xrxww' },
+  { id: 'rostro-pollita', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEiCwzzqZeJ5CzkvHN3u3pD0-nim7uDxANXmlLj-_bty5BrF4XyXFO_gwBZRRP4dIjrcOElk3L9iynGGCTsC7p1H8QpkBV8dqHFHKmp4BE8IbzUyqYMxKGinexGPny2O_xAu9W0d8-RAbk--trHN01-1jk20MbuDO3ZfTmhbMkDHFArexTraBz-S9a96cbI' },
+  { id: 'pollo-surf', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEipz0PMfn1yclCyzbh6KIExTe2Je4I3TNZ9-Xoa2-Zl3lF5O-Y1Eg4dTukA9AlAo8zhZqta5k7Y212ClQH81WpLpLBn2ScYun7dLt-ww7lHuJ3MPfIYxumLhpY4911dO18vZc-fvglm6Wl1-04rhJhvOLxkeMUqOEH49sulGuHYY63pv3aajv7J0mF0n2k' },
+  { id: 'tortuga-sombrero', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEiEMjHeX1vuERqQZZv9N3TYv8yA6fTyc8QzK8YOKNr2w_KO4FeIfvF3nPGgTHuk6Uogcq6wyM0Mtl2kAbLm53HT7Pvz-5F413aMZvjjBdSoGFHFNu1U12qfSrjAkDTBjWCPOjKzhwJuzeMe-bRXFsnnaWlU8B36IH1n3e4mfhNJ2rVRz8PbUPWY3FtqdyQ' },
+  { id: 'extra-1', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEi6JjEG-z7N-vQ_iUWiiN9WYmStkFCrb8B1EHRhyI_x_0lr7Ou21cZptlwxkDMAmDRIyLYglDQMU4E5lP_eXTdP9td3_GS2xU0Fopq0RDk18f5f79GPEKO3WWA4w2kLul2RjxxaWKyr2zKyTOyhtyVB09IWpdKlnQtoTE8162_RVv7JWuXhT3R5DuhxO5A' },
+  { id: 'extra-2', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEj7DQDkZacs-MkVUP_crDGPlWTytM_ZFjNE63B7-Cab1pn45XQSRwEgv7642by18gisqrseFWseiz2uoJLUx9L_FFtnu42mMuio0U5QMtJRvob-lCM8MLQH0EcWuy7Z_NUDIB3kfWJZWllDiZpLn3Zjt2USPWF61ZjF8-5tF6pFA9g5X9tfG5rRm-xdL14' },
+  { id: 'extra-3', url: 'https://blogger.googleusercontent.com/img/a/AVvXsEgzSsNmLby9QLonXBbby1uN7BQUEB4iyucEtFq0tGRhyqKpxpwuaxN0kpDRzl4cmHDw13qIx3yOVJxSaVIXPr2SmGecP-gq-TKYLOKGO0rHblA3NIfw8xLJ7tBaXw3ocHgVWAfdsUMZ6tBBGgwUQpwLWk0i7bZ1vVqzZ-FPGiLQca2hFto8WAqR-PkxKsw' }
+];
