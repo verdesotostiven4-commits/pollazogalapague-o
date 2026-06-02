29565c6fbfa531f0bed038d325f36735b004c0b5
verdesotostiven4-commits <verdesotostiven4@gmail.com>
2026-06-02 01:28:56 -0500

    Enable safe visual translations on app mount

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
+import { installHomeVisualTranslator } from './utils/homeVisualTranslator';
+
+installHomeVisualTranslator();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
